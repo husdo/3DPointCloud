5fcd497a39b5d265b5bc8407a9a8713861cdb572
Domonkos Huszar <huszardomonkos@gmail.com>
2015-07-06 13:44:21 +0100

    camera picture added to bagread, ShowPointCloud used instead of plot3k.

--- Matlab/LidarDataDisplay.m
+++ Matlab/LidarDataDisplay.m
@@ -3,20 +3,20 @@ function Pitch = LidarDataDisplay(DataFile,Params)
 if (nargin<2)
 	Params.HorizontalStep = 1;
 	Params.VerticalStep = 1;
-	Params.MinimalDistance = 0.4;
-	Params.MaximalDistance = 6;
+	Params.MinimalDistance = 0.5;
+	Params.MaximalDistance = 20;
 	Params.DistanceFromAxis = 0.075;
 	%Horizontal selection (in degree)
-	Params.StartAngleHor = -110;
-	Params.FinishAngleHor = 110;
+	Params.StartAngleHor = -70;
+	Params.FinishAngleHor = 70;
 
 	%vertical selection 0 z axis (in degree)
-	Params.StartAngleVer = 0;
+	Params.StartAngleVer = 30;
 	Params.FinishAngleVer = 180;
 end
-
+CameraData = [];
 if(strcmp(DataFile(end-2:end),'bag'))
-	[IMUData, LidarData] = bagRead(DataFile);
+	[IMUData, LidarData, CameraData] = bagRead(DataFile);
 elseif(strcmp(DataFile(end-2:end),'mat'))
 	load(DataFile);
 else
@@ -38,15 +38,19 @@ x = [];
 y = [];
 z = [];
 Colours = [];
+% if(numel(CameraData)>0);
+% 	figure(2);
+% 	imshow(CameraData.MiddlePicture);
+% end
 
-figure(1);
+PointCloudHandle = figure(1);
 
 zeroVector = [10 0 0];
 orientationTimeStamp = IMUData.TimeStamp;
 
 if(isfield(IMUData,'Orientation'));
 	orientationMatrix = IMUData.LinearAcceleration;
-	%orientationMatrix = IMUData.Orientation;
+	orientationMatrix = IMUData.Orientation;
 	orientation = 1;
 else
 	orientation = 0;
@@ -54,19 +58,25 @@ end
 
 for i = 1:Params.VerticalStep:size(LidarData.Ranges,1)
 	i/size(LidarData.Ranges,1) *100
-	[~, idx] = min(abs(orientationTimeStamp - LidarData.TimeStamp(i)));
+	[~, ImuIdx] = min(abs(orientationTimeStamp - LidarData.TimeStamp(i)));
+	
+% 	if(numel(CameraData)>0);
+% 		[~, CameraIdx] = min(abs(CameraData.TimeStamp - LidarData.TimeStamp(i)));
+% 		figure(3);
+% 		img = zeros(1,640,3);
+% 		img(:,:,:) = CameraData.Slices(CameraIdx,:,:,:);
+% 		imshow(img);
+% 	end
+	
 	
 	if(orientation)
-% 		scalar = [10 0 0]...
-% 			*[orientationMatrix(idx,1) orientationMatrix(idx,2) orientationMatrix(idx,3)]';
-% 		norm_base = norm([10 0 0]);
-% 		norm_current = norm([orientationMatrix(idx,1) orientationMatrix(idx,2) orientationMatrix(idx,3)]);
-% 		pitch = acos(scalar/(norm_base*norm_current));
-% 		pitch = atan2(orientationMatrix(idx,3),orientationMatrix(idx,1));
-	anyad = (SpinCalc('QtoEA321', IMUData.Orientation(idx,:), 1, 1));
-	pitch = deg2rad(anyad(2) +90);
+		EulerAngles = SpinCalc('QtoEA321',orientationMatrix(ImuIdx,:),1,1);
+		pitch = deg2rad(EulerAngles(2)+90);
+		if(pitch>2*pi)
+			pitch = pitch - 2*pi;
+		end
 	else
-		pitch = IMUData.Angle(idx,1);
+		pitch = IMUData.Angle(ImuIdx,1);
 	end
 	
 	Pitch(i) = pitch;
@@ -84,19 +94,45 @@ for i = 1:Params.VerticalStep:size(LidarData.Ranges,1)
 	x_tmp = distance.*cos(yaw)*sin(pitch) + Params.DistanceFromAxis *sin(pitch);
 	y_tmp = distance.*sin(yaw);%*sin(pitch);
 	z_tmp = distance.*cos(yaw)*cos(pitch)+ Params.DistanceFromAxis *cos(pitch);
-	Colours_tmp = z_tmp';
+	%Colours_tmp = z_tmp';
 	select = x_tmp~=0 | y_tmp~=0 | z_tmp~=0;
 	x_tmp = x_tmp(select);	
 	y_tmp = y_tmp(select);
 	z_tmp = z_tmp(select);
+	d = d(select);
 	Colours_tmp = Colours_tmp(select,:);
  	x = [x x_tmp];
  	y = [y y_tmp];
  	z = [z z_tmp];
 	Colours = [Colours; Colours_tmp];
 	
+	refreshRate = 5;
+	
+	if(mod(i,refreshRate) == 0)
+		showPointCloud([x(:),y(:),z(:),],Colours);
+		
+		xlim([-3 15])
+		ylim([-15 15])
+		zlim([-2 8])
+		if(i<700)
+			view(-30,5);
+		elseif (i<900)
+			view(-1*((70-30)/200*(i-700)+30),5);
+		else
+			view(-70,5)
+		end
+		if (i == refreshRate)
+			pause;
+		end
+
+		drawnow;
+		colormap(PointCloudHandle,'jet');
+		%saveas(PointCloudHandle,['video',num2str(i),'.jpg']);
+	end
+	
 end
 	
 L = [x' y' z'];
-plot3k(L, 'ColorData', Colours(:,1));
+showPointCloud([x(:),y(:),z(:)]);
+%plot3k(L, 'ColorData', Colours(:,1));
 axis equal
--- Matlab/bagRead.m
+++ Matlab/bagRead.m
@@ -1,4 +1,4 @@
-function [IMUData, LidarData] = bagRead(fileName, saveFile)
+function [IMUData, LidarData, CameraData] = bagRead(fileName, saveFile)
 
 if(nargin<2)
 	saveFile = 0;
@@ -8,9 +8,11 @@ bag = rosbag(fileName);
 
 bagselectIMU = select(bag, 'Topic', '/mavros/imu/data');
 bagselectLidar = select(bag, 'Topic', '/most_intense');
+bagselectCamera = select(bag, 'Topic', '/usb_cam/image_raw/compressed');
 
 IMUCell = readMessages(bagselectIMU);
 LidarCell = readMessages(bagselectLidar);
+CameraCell = readMessages(bagselectCamera);
 
 LidarData = [];
 LidarData.Ranges = zeros(numel(LidarCell),numel(LidarCell{1}.Ranges));
@@ -25,6 +27,7 @@ LidarData.AngleMin = LidarCell{1}.AngleMin;
 LidarData.AngleIncrement = LidarCell{1}.AngleIncrement;
 LidarData.RangeMin = LidarCell{1}.RangeMin;
 LidarData.RangeMax = LidarCell{1}.RangeMax;
+LidarData.ScanTime = LidarCell{1}.ScanTime;
 	
 for IMUIterator = 1:numel(IMUCell)
 	IMUData.Orientation(IMUIterator,:) = [IMUCell{IMUIterator}.Orientation.X IMUCell{IMUIterator}.Orientation.Y IMUCell{IMUIterator}.Orientation.Z IMUCell{IMUIterator}.Orientation.W];
@@ -33,6 +36,15 @@ for IMUIterator = 1:numel(IMUCell)
 end
 IMUData.TimeStamp = bagselectIMU.MessageList.Time;
 
+CameraData.MiddlePicture = readImage(CameraCell{round(numel(CameraCell)/2)});
+
+% for CameraIterator = 1:numel(CameraCell)
+%  	img = readImage(CameraCell{CameraIterator});
+%  	CameraData.Slices(CameraIterator,:,:,:) = img(120,:,:);
+% 	CameraIterator
+% end
+CameraData.TimeStamp = bagselectCamera.MessageList.Time;
+
 if(saveFile)
-	save(fileName(1:end-4),'IMUData','LidarData');
+	save(fileName(1:end-4),'IMUData','LidarData','CameraData');
 end
